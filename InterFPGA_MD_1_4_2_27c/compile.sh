bc37fa2c039c447cbfaff9530a3ea60acacc1256
ChunshuWu <chunshu.wu.bu@gmail.com>
2023-06-26 22:25:18 -0400

    Initial release

--- InterFPGA_MD_1_4_2_27c/compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-make all DEVICE=xilinx_u280_xdma_201920_3 INTERFACE=0 DESIGN=benchmark
--- InterFPGA_MD_1_4_2_27c_bitstream/upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ssh -o "ProxyCommand ssh -i ~/Documents/pvtkey -W %h:%p <username>@<server_name>" -i ~/Documents/pvtkey <username>@$1.cloudlab.umass.edu 'mkdir ~/benchmark.intf0.xilinx_u280_xdma_201920_3'
-#source /opt/xilinx/xrt/setup.sh
-scp -o "ProxyCommand ssh -i ~/Documents/pvtkey <username>@<server_name> nc %h %p" -i ~/Documents/pvtkey benchmark.intf0.xilinx_u280_xdma_201920_3/vnx_benchmark_if0.xclbin <username>@$1.cloudlab.umass.edu:~/benchmark.intf0.xilinx_u280_xdma_201920_3/vnx_benchmark_if0.xclbin
-scp -o "ProxyCommand ssh -i ~/Documents/pvtkey <username>@<server_name> nc %h %p" -i ~/Documents/pvtkey -r Notebooks <username>@$1.cloudlab.umass.edu:~
